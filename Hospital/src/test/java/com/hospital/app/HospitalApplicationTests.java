70eaea24ab74cf858bf469487fb838cd02ca234a
Dweepikav <dweepikareddy2000@gmail.com>
2022-01-21 14:17:34 +0530

    deepu1

---
+++ Hospital/src/test/java/com/hospital/app/HospitalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hospital.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1,1);
+	}
+
+}
